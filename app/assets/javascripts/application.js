f81a1966b7373a628ecea8061f60706f9cb39d50
PJLvadas <plivadas@gmail.com>
2015-05-12 15:34:48 -0400

    latest version for issue help

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,77 +33,92 @@ var App = {
 
 $(function() {
 	console.log('APP UP');
-	
+	$('#calc').on('click', 'button', calcRoute);
+
 	var mapOptions = {
 		zoom: 5,
 		center: new google.maps.LatLng(40, -80.5),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	var routeBoxer = new RouteBoxer();
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+	var directionsService = new google.maps.DirectionsService();	
+
+
+	var calcRoute = function() {
+	debugger;
+		//RouteBoxer
+		clearBoxes();
+
+		//Directions Request
+		var start = document.getElementById('start').value;
+		var end   = document.getElementById('end').value;
+		var request = {
+			  origin: start,
+			  destination: end,
+			  travelMode: google.maps.TravelMode.DRIVING
+			};
+
+		directionsDisplay.setMap(map);
+		directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+		directionsService.route(request, function(response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+				
+				var distance = parseFloat(1/20);
+				var path = response.routes[0].overview_path;
+				var boxes = routeBoxer.box(path, distance);
+				drawBoxes(boxes);
+
+				// for (var i = 0; i < boxes.length; i++) {
+				// 		var nBound = boxes[i].Ea.j;
+				// 		var sBound = boxes[i].Ea.A;
+				// 		var wBound = boxes[i].wa.j;
+				// 		var eBound = boxes[i].wa.A;
+				// 	if ( nBound < tollLat && sBound > tollLat && wBound < tollLng && eBound > tollLng ) {
+						
+				// 	}
+				// }
+			} 
+			else { alert("Directions query failed: " + status);
+			}
+		});
 	};
-	
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	$('#calc').on('click', 'button', calcRoute);
-
-	// App.plazasCollection = new App.Collections.Plazas();
-	// App.plazasCollection.fetch();
-	// App.tollsCollection = new App.Collections.Tolls();
-	// App.tollsCollection.fetch();
-
-	// App.router = new App.router();
-	// Backbone.history.start();
-});
-
-var calcRoute = function() {
-    clearBoxes();
-	
-	//Marker
-    var myLatlng = new google.maps.LatLng(39.682395, -75.498417)		
-	marker = new google.maps.Marker({
-   	 	position: myLatlng,
-    	title:"Hello World!"
-	});
-
-	//RouteBoxer
-	routeBoxer = new RouteBoxer();
-
-
-	//Directions Request
-	var start = document.getElementById('start').value;
-	var end   = document.getElementById('end').value;
-	var request = {
-		origin: start,
-		destination: end,
-		travelMode: google.maps.TravelMode.DRIVING
-	};
-	directionsDisplay = new google.maps.DirectionsRenderer();
-	directionsDisplay.setMap(map);
-	directionsDisplay.setPanel(document.getElementById('directions-panel'));
-
-	directionsService = new google.maps.DirectionsService();
-	directionsService.route(request, function(response, status) {
-		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(response);
-			
-			var distance = parseFloat(1/20);
-			var path = response.routes[0].overview_path;
-			var boxes = routeBoxer.box(path, distance);
-			drawBoxes(boxes);
-			// for (var i = 0; i < boxes.length; i++) {
-			// 	var bounds = boxes[i];
-			//  marker.setMap(map);
-			// }
-			// for (var i = 0; i < boxes.length; i++) {
-			// 	if ( 
-			// 		   boxes[i].Ea.j < tollLat
-			// 		&& boxes[i].Ea.A > tollLat
-			// 		&& boxes[i].wa.j < tollLng
-			// 		&& boxes[i].wa.A > tollLng )
-			// }
-		} else {
-			alert("Directions query failed: " + status);
+	var drawBoxes = function(boxArray) {
+		boxpolys = new Array(boxArray.length);
+		for (var i = 0; i < boxArray.length; i++) {
+			boxpolys[i] = new google.maps.Rectangle({
+				bounds: boxArray[i],
+				fillOpacity: 0,
+				strokeOpacity: 1.0,
+				strokeColor: '#000000',
+				strokeWeight: 1,
+				map: map
+			});
 		}
-	});
-}
+	};
+
+	var clearBoxes = function() {
+		if (boxpolys != null) {
+			for (var i = 0; i < boxpolys.length; i++ ) {
+				boxpolys[i].setMap(null);
+			};
+		}
+		boxpolys = null;
+	};
+
+	var tollCall = function() {
+		$.get('/tolls')
+		.done(function(tolls) {
+
+		})
+	};
+
+
+});
 
 
 
--- app/controllers/tolls_controller.rb
+++ app/controllers/tolls_controller.rb
@@ -1,5 +1,65 @@
 class TollsController < ApplicationController
   
+  def index
+  	@tolls = Toll.all
 
+  	respond_to do |format|
+  		format.html{}
+  		format.json { render json: @tolls }
+  	end
+  end
+
+  def new
+  	@toll = Toll.new
+  end
+
+  def create
+  	@toll = Toll.create(toll_params)
+  	@toll.save
+
+  	respond_to do |format|
+  		format.html { redirect_to "/tolls"}
+  		format.json { render json: @toll }
+  	end
+  end
+
+  def show
+  	@toll = Toll.find(params[:id])
+
+  	respond_to do |format|
+  		format.html {}
+  		format.json { render json: @toll }
+  	end
+  end
+
+  def edit
+  	@toll = Toll.find(params[:id])
+  end
+
+  def update
+  	@toll = Toll.find(params[:id])
+  	@toll.update(toll_params)
+
+  	respond_to do |format|
+  		format.html { redirect to "/tolls/#{@toll.id}" }
+  		format.html { render json: @toll }
+  	end
+  end
+
+  def destroy 
+  	@toll = Toll.find(params[:id])
+  	@toll.destroy
+
+  	respond_to do |format|
+  		format.html { redirect_to '/tolls' }
+  		format.json { render json: @toll }
+  	end
+  end
+
+  private
+
+  def toll_params
+  	params.require(:toll).permit(:name, :state, :description, :latitude, :longitude, :amount, :ez_pass, :on_route)
+  end
 
 end
