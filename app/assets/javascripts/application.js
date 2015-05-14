f4638ae292636e75b7aae006fb22987b3f6ba4ba
PJLvadas <plivadas@gmail.com>
2015-05-13 22:26:18 -0400

    all set, css is acceptable. functioning for presentation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,45 +40,57 @@ $(function() {
 	$('#show-tolls').on('click', tollCall);
 	$('#toll-bar').hide;
 
+	// Sets default map view
 	var mapOptions = {
 		zoom: 5,
-		center: new google.maps.LatLng(40, -80.5),
+		center: new google.maps.LatLng(38.803057, -80.527466),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
+
+	//Grabs div in which map will be displayed, provides default view
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);	
 
 });
 
 	var calcRoute = function() {
-		// var allTolls = tollCall();
+		
+		// Defining the necessary variables for GoogleMaps and RouteBoxer
 		var routeBoxer = new RouteBoxer();
 		var directionsDisplay = new google.maps.DirectionsRenderer();
 		var directionsService = new google.maps.DirectionsService();
 	
-		//RouteBoxer
+		//RouteBoxer function
 		clearBoxes();
 
-		//Directions Request
+		//Directions request begins by grabbing user input values
 		start = document.getElementById('start').value;
 		end   = document.getElementById('end').value;
+		
+		//Necessary paramaters for directions request
 		var request = {
 			  origin: start,
 			  destination: end,
 			  travelMode: google.maps.TravelMode.DRIVING
 			};
 
+		//Sets the div in which the map with directions will be rendered
 		directionsDisplay.setMap(map);
+
+		//Sets the div in which the directions text will be rendered
 		directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
+		//Directions function
 		directionsService.route(request, function(response, status) {
 			if (status == google.maps.DirectionsStatus.OK) {
 				directionsDisplay.setDirections(response);
-				
+			
+			//Variables set to determine direction of the route
 				originLat = response.routes[0].legs[0].start_location.A
 				originLng = response.routes[0].legs[0].start_location.F
 				destLat = response.routes[0].legs[0].end_location.A
 				destLng = response.routes[0].legs[0].end_location.F
 
+			//Variables set for box calculations and drawing
 				var distance = parseFloat(1/10);
 				var path = response.routes[0].overview_path;
 				boxes = routeBoxer.box(path, distance);
@@ -90,13 +102,15 @@ $(function() {
 		});
 	};
 
+// Draws boxes covering the entire route
+// Box coordinates and size are calculated during the Directions Service request
 	var drawBoxes = function(boxArray) {
 		boxpolys = new Array(boxArray.length);
 		for (var i = 0; i < boxArray.length; i++) {
 			boxpolys[i] = new google.maps.Rectangle({
 				bounds: boxArray[i],
 				fillOpacity: 0,
-				strokeOpacity: 1.0,
+				strokeOpacity: .7,
 				strokeColor: '#000000',
 				strokeWeight: 1,
 				map: map
@@ -113,19 +127,27 @@ $(function() {
 		boxpolys = null;
 	};
 	
+	//Setting empty arrays for future logic and calculations
 	var tollArr = []
 	var tollAmtS = []
 	var tollAmtN = []
 	var tollBar = document.getElementById('toll-bar');
+	
+	//Retrives toll information from the database
 	var tollCall = function() {
 		$.get('/tolls')
 		 .success(function(tolls) {
+		
+		// Logic to determine if a toll exists within the route
+		// Determines if the toll lat/long are within the boundaries of any box
 			for (var t = 0; t < tolls.length; t++) {
 			  for (var i = 0; i < boxes.length; i++) {
 			    if ( boxes[i].Ea.j > tolls[t].latitude 
 			      && boxes[i].Ea.A < tolls[t].latitude 
 			      && boxes[i].va.j < tolls[t].longitude 
 			      && boxes[i].va.A > tolls[t].longitude ){
+			    //Dividing info into pertinent arrays
+			    //Important to separate tolls paid going north vs. south	
 			    	tollArr.push(tolls[t]);
 			    	tollAmtS.push(tolls[t].s_amount);
 			    	tollAmtN.push(tolls[t].n_amount);
@@ -134,26 +156,27 @@ $(function() {
 			  }
 			};
 
+			//Logic for displaying toll amounts based on route direction
 			for (var a = 0; a < tollArr.length; a++) {
 				if (originLat > destLat ) {
 				$('#toll-bar').append('<ul>'+tollArr[a].name+'</ul>'+'<li>Southbound: $'+tollArr[a].s_amount+'</li>')	
 				} else { $('#toll-bar').append('<ul>'+tollArr[a].name+'</ul>'+'<li>Northbound: $'+tollArr[a].n_amount+'</li>') }
 		    
+		    //Adds pins to tolls existing on the route
 			    marker = new google.maps.Marker({
 			    	position: new google.maps.LatLng(tollArr[a].latitude, tollArr[a].longitude),
 			        map: map
 			    });
 		    };
 
-
+		    //Calculating sum of all tolls paid, appending to div
 		    var total = 0;	
 			for (var b = 0; b < tollArr.length; b++ ) {
 				if (originLat > destLat ) {
 					total += tollArr[b].s_amount;
 				} else { total += tollArr[b].n_amount }
 			};
-				$('#toll-bar').prepend('<ul id="toll-total">Total: $'+total+'</sec>')
-
+				$('#toll-bar').prepend('<ul id="toll-total">Total: $'+total+'</ul>')
 				
 		});
 	};
